7c1d12b4a08785f9cd1443331d91e860f71a0668
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-06 17:16:13 +0300

    Getting Data's from Firebase

--- app/src/main/java/com/yusuf/dictionary/ui/DictionaryPageFragment.kt
+++ app/src/main/java/com/yusuf/dictionary/ui/DictionaryPageFragment.kt
@@ -1,13 +1,20 @@
 package com.yusuf.dictionary.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.yusuf.dictionary.R
 import com.yusuf.dictionary.adapter.WordAdapter
 import com.yusuf.dictionary.databinding.FragmentDictionaryPageBinding
@@ -19,6 +26,8 @@ class DictionaryPageFragment : Fragment() {
     lateinit var binding: FragmentDictionaryPageBinding
     lateinit var wordList: ArrayList<Word>
     lateinit var adapter: WordAdapter
+
+    private lateinit var wordReference : DatabaseReference
 
     private lateinit var dao: WordDao
 
@@ -43,7 +52,16 @@ class DictionaryPageFragment : Fragment() {
 
         super.onViewCreated(view, savedInstanceState)
         dao = WordDao(requireContext())
-        wordList = dao.getAllWords() as ArrayList<Word>
+        //wordList = dao.getAllWords() as ArrayList<Word>
+
+        wordList = ArrayList()
+
+        val db = FirebaseDatabase.getInstance()
+        wordReference = db.getReference("kelimeler")
+
+        Log.e("datas",wordReference.toString())
+
+        observeDatas()
 
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
@@ -51,8 +69,38 @@ class DictionaryPageFragment : Fragment() {
         binding.recyclerView.adapter = adapter
 
 
+
+        Log.e("datas",wordList.toString())
+
     }
 
+    private fun observeDatas(){
+        wordReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                wordList.clear()
+
+                for (c in snapshot.children){
+                    val word = c.getValue(Word::class.java)
+                    if (word != null){
+                        Log.e("datas",word.toString())
+                        word.kelime_id = c.key
+                        wordList.add(word)
+
+                    }
+
+                }
+
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"Something went wrong!",Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
 
 
 }
